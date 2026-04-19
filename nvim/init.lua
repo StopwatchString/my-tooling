7a2ee50ccbf89b26dafb915f28a83db0da346347
Mason Speck <mcs.5299@gmail.com>
2026-04-19 02:50:51 -0500

    smarter restart, tries to preserve state

--- nvim/init.lua
+++ nvim/init.lua
@@ -44,8 +44,12 @@ vim.keymap.set('n', '<leader>pr', function()
   vim.cmd.Ex(root)
 end)
 
--- Restart
-vim.keymap.set('n', '<leader>restart', ':restart<CR>')
+-- Inplace restart
+vim.keymap.set('n', '<leader>R', function()
+  local session = vim.fn.stdpath('state') .. '/restart_session.vim'
+  vim.cmd('mksession! ' .. vim.fn.fnameescape(session))
+  vim.cmd('restart source ' .. vim.fn.fnameescape(session))
+end, { desc = 'Restart Neovim' })
 
 -- Open directory of current file
 vim.keymap.set("n", "<leader>.", function()
