7fb18c84dbdb01f158eae75a2e23e59697b766a3
Mason Speck <mcs.5299@gmail.com>
2026-04-29 20:44:24 -0500

    Bookmark lookup list

--- nvim/lua/bookmarks.lua
+++ nvim/lua/bookmarks.lua
@@ -1,5 +1,29 @@
 local M = {}
 
+function M.list()
+  local marks = {}
+  for i = 0, 9 do
+    local mark = string.char(string.byte('A') + i)
+    local info = vim.fn.getpos("'" .. mark)
+    local lnum = info[2]
+    local file = vim.fn.bufname(info[1])
+    if lnum ~= 0 then
+      table.insert(marks, { mark = mark, lnum = lnum, file = file, index = i })
+    end
+  end
+
+  vim.ui.select(marks, {
+    prompt = 'Bookmarks',
+    format_item = function(item)
+      return string.format('[%d] %s — line %d', item.index, item.file, item.lnum)
+    end,
+  }, function(choice)
+    if choice then
+      vim.cmd("normal! '" .. choice.mark)
+    end
+  end)
+end
+
 function M.setup()
     local set_marks = '!@#$%^&*()'
     local go_marks  = '1234567890'  -- unshifted versions
@@ -17,6 +41,8 @@ function M.setup()
             vim.cmd("normal! '" .. mark)
         end, { desc = 'Go to mark ' .. mark })
     end
+
+    vim.keymap.set('n', '<leader>b', M.list)
 end
 
 return M
